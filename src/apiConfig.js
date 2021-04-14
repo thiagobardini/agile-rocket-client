0b44eac588eb06f168e28ade20d5128ff08a9696
thiagobadini <thiagobardini@hotmail.com>
2021-04-13 22:36:52 -0400

    ADD heroku addreess - production

--- src/apiConfig.js
+++ src/apiConfig.js
@@ -1,6 +1,6 @@
 let apiUrl
 const apiUrls = {
-  production: 'https://aqueous-atoll-85096.herokuapp.com',
+  production: 'https://mysterious-sierra-58663.herokuapp.com/',
   development: 'http://localhost:4741'
 }
 
