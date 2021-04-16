76924ac3253bcc5b3f743806edb4a91747074b46
thiagobadini <thiagobardini@hotmail.com>
2021-04-16 12:58:01 -0400

    Added populate() userName from user to the account

--- src/api/auth.js
+++ src/api/auth.js
@@ -7,6 +7,7 @@ export const signUp = credentials => {
     url: apiUrl + '/sign-up',
     data: {
       credentials: {
+        userName: credentials.userName,
         email: credentials.email,
         password: credentials.password,
         password_confirmation: credentials.passwordConfirmation
