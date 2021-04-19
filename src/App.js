6c4e084a0832d7eb62d16e767f994cb07841bbbb
thiagobadini <thiagobardini@hotmail.com>
2021-04-19 09:51:03 -0400

    Design

--- src/App.js
+++ src/App.js
@@ -1,84 +1,119 @@
-import React, { Component } from 'react'
-import { withRouter, Link } from 'react-router-dom'
-import { accountIndex } from '../../api/accounts'
-import Spinner from 'react-bootstrap/Spinner'
+import React, { Component, Fragment } from 'react'
+import { Route } from 'react-router-dom'
+import { v4 as uuid } from 'uuid'
 
-const cardContainerLayout = {
-  display: 'flex',
-  flexDirection: 'column',
-  alignItems: 'baseline'
-}
-class HomeAccountPage extends Component {
+import AuthenticatedRoute from './components/AuthenticatedRoute/AuthenticatedRoute'
+import AutoDismissAlert from './components/AutoDismissAlert/AutoDismissAlert'
+import Header from './components/Header/Header'
+import SignUp from './components/SignUp/SignUp'
+import SignIn from './components/SignIn/SignIn'
+import SignOut from './components/SignOut/SignOut'
+import ChangePassword from './components/ChangePassword/ChangePassword'
+
+// IMPORT ACCOUNT CRUD COMPONENTS
+import AccountCreate from './components/AccountRoutes/CreateAccount'
+import AccountIndex from './components/AccountRoutes/IndexAccount'
+import AccountShow from './components/AccountRoutes/ShowAccount'
+import AccountUpdate from './components/AccountRoutes/UpdateAccount'
+import HomePageAccount from './components/AccountRoutes/HomePageAccount'
+import './index.scss'
+
+// OPPORTUNITIES COMPONENTS
+import OpportunityCreate from './components/OpportunityRoutes/CreateOpportunity'
+// import OpportunityUpdate from './components/OpportunityRoutes/UpdateOpportunity'
+
+class App extends Component {
   constructor (props) {
     super(props)
     this.state = {
-      accounts: null
+      user: null,
+      msgAlerts: []
     }
   }
-  componentDidMount () {
-    const { msgAlert, user } = this.props
-    accountIndex(user)
-      .then(res => this.setState({ accounts: res.data.accounts }))
-      .then(() => msgAlert({
-        heading: 'Loaded accounts!',
-        message: 'All accounts ',
-        variant: 'success'
-      }))
-      .catch(error => {
-        msgAlert({
-          heading: 'Failed to load the accounts!',
-          message: 'the posts have an error' + error.message,
-          variant: 'danger'
-        })
-      })
+
+  setUser = user => this.setState({ user })
+
+  clearUser = () => this.setState({ user: null })
+
+  deleteAlert = (id) => {
+    this.setState((state) => {
+      return { msgAlerts: state.msgAlerts.filter(msg => msg.id !== id) }
+    })
   }
+
+  msgAlert = ({ heading, message, variant }) => {
+    const id = uuid()
+    this.setState((state) => {
+      return { msgAlerts: [...state.msgAlerts, { heading, message, variant, id }] }
+    })
+  }
+
   render () {
-    function getRandomAccount (max) {
-      return Math.floor(Math.random() * Math.floor(max))
-    }
-    const { accounts } = this.state
-    if (!accounts) {
-      return (
-        <Spinner animation="border" role="status">
-          <span className="sr-only">loading</span>
-        </Spinner>
-      )
-    }
-    const accountJsx = accounts.map(account => (
-      <div className="card" key={account._id}>
-        <div className="card-body">
-          <Link to={`/accounts/${account._id}`} key={account._id}><h4 className="card-title">{account.accountName}</h4></Link>
-          <p className="card-text">{account.industry}</p>
-          <p className="card-text">{account.contact}</p>
-          <p className="card-text">{account.email}</p>
-          <p className="card-text">{account.address}</p>
-          <p className="card-text">{account.phone}</p>
-          <p className="card-text"><small className="text-muted">Created: {account.dateCreated.substring(0, 10)}</small></p>
-        </div>
-      </div>
-    ))
+    const { msgAlerts, user } = this.state
+
     return (
-      <div style={cardContainerLayout}>
-        <div className="container" id="indexPage-message"><h3 className="h3Indexstyle">Recently Accounts!</h3>
-          <div className="row">
-            <div className="col-md">
-              <h6>{accountJsx[getRandomAccount(accountJsx.length)]}</h6>
+      <Fragment>
+        <Header user={user} />
+        {msgAlerts.map(msgAlert => (
+          <AutoDismissAlert
+            key={msgAlert.id}
+            heading={msgAlert.heading}
+            variant={msgAlert.variant}
+            message={msgAlert.message}
+            id={msgAlert.id}
+            deleteAlert={this.deleteAlert}
+          />
+        ))}
+        <main className="container">
+          <Route path='/sign-up' render={() => (
+            <SignUp msgAlert={this.msgAlert} setUser={this.setUser} />
+          )} />
+          <Route path='/sign-in' render={() => (
+            <SignIn msgAlert={this.msgAlert} setUser={this.setUser} />
+          )} />
+          <AuthenticatedRoute user={user} path='/sign-out' render={() => (
+            <SignOut msgAlert={this.msgAlert} clearUser={this.clearUser} user={user} />
+          )} />
+          <AuthenticatedRoute user={user} path='/change-password' render={() => (
+            <ChangePassword msgAlert={this.msgAlert} user={user} />
+          )} />
+          <AuthenticatedRoute user={user} exact path='/account-create' render={() => (
+            <AccountCreate msgAlert={this.msgAlert} user={user} />
+          )} />
+          <AuthenticatedRoute user={user} exact path='/accounts' render={() => (
+            <AccountIndex msgAlert={this.msgAlert} user={user} />
+          )} />
+          <Route user={user} exact path='/accounts/:id' render={() => (
+            <AccountShow msgAlert={this.msgAlert} user={user} />
+          )} />
+          <AuthenticatedRoute user={user} exact path='/accounts/:id/edit' render={() => (
+            <AccountUpdate msgAlert={this.msgAlert} user={user} />
+          )}/>
+          <Route exact path='/' render={() => (
+            <div className="jumbotron">
+              <h1 className="display-4">Welcome!</h1>
+              <p className="lead">Agile Rocket -  CRM</p>
+              <hr className="my-4"/>
             </div>
-            <div className="col-md">
-              <h6>{accountJsx[getRandomAccount(accountJsx.length)]}</h6>
-            </div>
-          </div>
-          <div className="row">
-            <div className="col-md">
-              <h6>{accountJsx[getRandomAccount(accountJsx.length)]}</h6>
-            </div>
-            <div className="col-md">
-              <h6>{accountJsx[getRandomAccount(accountJsx.length)]}</h6>
-            </div>
+          )}/>
+          <AuthenticatedRoute user={user} exact path='/' render={() => (
+            <HomePageAccount msgAlert={this.msgAlert} />
+          )}/>
+          {/* <AuthenticatedRoute user={user} path='/accounts/:id/opportunity-update' render={() => (
+            <OpportunityUpdate msgAlert={this.msgAlert} user={user} />
+          )} /> */}
+          <AuthenticatedRoute user={user} exact path='/accounts/:id/opportunity-create' render={() => (
+            <OpportunityCreate msgAlert={this.msgAlert} user={user} />
+          )} />
+        </main>
+        <div className="jumbotron jumbotron-fluid fixed-bottom footer-group mt-5">
+          <div className="container-fluid">
+            <p>Copyright &copy; 2021 - by <a href="https://github.com/thiagobardini" className="button gitRepo" data-abc="true">Thiago Bardini</a></p>
           </div>
         </div>
-      </div>
+      </Fragment>
     )
   }
 }
-export default withRouter(HomeAccountPage)
+
+export default App
