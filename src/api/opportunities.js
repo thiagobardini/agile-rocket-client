02ab52acbdd312ec1957f1ca894df0911802dc09
thiagobadini <thiagobardini@hotmail.com>
2021-11-24 13:26:38 -0500

    Fixed create opportunity

--- src/api/opportunities.js
+++ src/api/opportunities.js
@@ -4,10 +4,10 @@ import apiUrl from '../apiConfig'
 export const opportunityCreate = (opportunity, user, id) => {
   // console.log('created opportunity is :', opportunity.opportunityName)
   return axios({
-    url: apiUrl + '/accounts/' + id + '/opportunity-create',
+    url: apiUrl + '/accounts/' + id + '/opportunities',
     method: 'POST',
     headers: {
-      'Authorization': `Bearer ${user.token}`
+      Authorization: `Bearer ${user.token}`
     },
     data: { opportunity }
   })
@@ -16,7 +16,7 @@ export const opportunityCreate = (opportunity, user, id) => {
 // UPDATE
 export const opportunityUpdate = (id, opportunity, user) => {
   return axios({
-    url: apiUrl + '/accounts/' + id + '/opportunity-update',
+    url: apiUrl + '/accounts/' + id + '/opportunities',
     method: 'PATCH',
     headers: {
       'Authorization': `Bearer ${user.token}`
